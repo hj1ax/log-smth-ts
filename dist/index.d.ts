ec2467853676f20ac2465f0b817f658195bae360
ya-sach1 <ikurogane85@gmail.com>
2020-09-02 01:23:50 +0200

    helo

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Levels, loggerOptions } from './structures/utils';
-export declare class Logger {
-    loggerOptions: loggerOptions;
-    constructor(options?: loggerOptions);
-    log(message: string, loglevel: string | Levels): void;
-}
