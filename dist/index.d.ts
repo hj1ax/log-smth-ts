412ad183c89cfca4a64fd99e9aabc46970cede29
Jiaxing Hu <jiaxinghu52@gmail.com>
2020-10-01 02:24:42 +0200

    Removed dist/ folder.

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Levels, loggerOptions } from './structures/utils';
-export default class Logger {
-    private _loggerOptions;
-    constructor(options?: loggerOptions);
-    log(message: string, loglevel?: string | Levels): void;
-}
--- dist/structures/functions.d.ts
+++
@@ -1,6 +0,0 @@
-import { Chalk } from 'chalk';
-import { Levels, logSkel } from './utils';
-declare const sendLog: (color: Chalk, message: string, level: Levels | string, symbol: string, timestamp: Date | string) => void;
-declare const firstLetterToUppercase: (text: string | Levels) => string;
-declare const logInFile: (path: string, data: logSkel) => void;
-export { logInFile, firstLetterToUppercase, sendLog };
